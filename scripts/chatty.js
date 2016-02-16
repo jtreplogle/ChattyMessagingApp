d4e41f651db8e97dc4f41f1375c49a8bd480c81f
Erin Orstrom <eorstrom2010@gmail.com>
2016-02-16 13:33:04 -0600

    added many comments

--- scripts/chatty.js
+++ scripts/chatty.js
@@ -3,17 +3,20 @@ var FetchMessage = (function () {
     
     return {
         loadMessages: function () {
+            // Create new XHR request
             var myRequest = new XMLHttpRequest();
 
             myRequest.addEventListener("load", function() {
+                // Parse JSON objects into a native JavaScript Object
                 var messageData = JSON.parse(this.responseText);
+                // Set private variable equal to the native JavaScript object
                 messageArray = messageData.messages;
                 console.log(messageArray);
-                // messageArray.forEach(function(currentObject){
-                //     FetchMessage.newMessage("chatBox", currentObject.msg)
-                // })
-                
-
+                /*
+                messageArray.forEach(function(currentObject){
+                    FetchMessage.newMessage("chatBox", currentObject.msg)
+                })
+                */
                 for (var i = 0; i < messageArray.length; i++) {
                     var currentObject = messageArray[i];
                     FetchMessage.newMessage("chatBox", currentObject.msg);
@@ -27,34 +30,3 @@ var FetchMessage = (function () {
 })();
 
 FetchMessage.loadMessages();
-
-/*
-return {
-
-        loadInventory: function (callback) {
-        // Create an XHR Request to load cars from CarLot.json file
-            var inventoryLoader = new XMLHttpRequest();
-
-        // Listen for when the load event is broadcast and execute an anonymous callback
-        inventoryLoader.addEventListener("load", function () {
-            // Set the value of the private array, and parse into a native JS object
-            var inventoryData = JSON.parse(this.responseText);
-            inventory = inventoryData.cars;
-            console.log("inventory", inventory);
-
-            callback(inventory);
-        });
-
-        // Retrieve the information in inventory.json
-        inventoryLoader.open("GET", "inventory.json");
-        inventoryLoader.send();
-    },
-
-        getInventory: function () {
-            return inventory
-        },
-    };
-*/
-
-
-
--- scripts/removeMessage.js
+++ scripts/removeMessage.js
@@ -1,5 +1,6 @@
 var FetchMessage = (function(message){
 
+    // function to delete selected message from DOM
     message.deleteMessage = function(messageId) {
         var messageToDelete = document.getElementById(messageId);
         messageToDelete.parentNode.removeChild(messageToDelete);
